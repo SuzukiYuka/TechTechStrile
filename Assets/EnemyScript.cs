ba38f8fc5113c518274c654bfe8a9736fb44eda4
Yuka Suzuki <suzukiyuka1218@gmail.com>
2017-09-14 08:51:44 +0900

    first commit

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+	public int enemyHP = 100;
+
+	void Start () {
+
+		Debug.Log ("敵のHPは" + enemyHP);
+	}
+	
+	void Update () {
+
+		if (enemyHP == 0) {
+
+			Debug.Log ("clear");
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision) {
+
+		enemyHP--;
+		Debug.Log ("敵のHPは" + enemyHP);
+	
+	}
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	int launchCount = 0;
+	int playerHP = 100;
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("プレイヤーHPは" + playerHP);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey ("up")) {
+
+			if (gameObject.GetComponent<Rigidbody>().velocity == new Vector3(0,0,0)) {
+		
+				gameObject.GetComponent<Rigidbody> ().velocity = transform.forward * 50f;
+				launchCount += 1;
+				Debug.Log (launchCount);
+			}
+		}
+
+		if (gameObject.GetComponent<Rigidbody>().velocity == new Vector3(0,0,0)) {
+
+			transform.Rotate (new Vector3 (0, 1f, 0));
+		}
+
+		if (playerHP == 0) {
+			Debug.Log ("GameOver");
+		}
+	}
+
+	private void OnTriggerEnter(Collider other) {
+	
+		playerHP--;
+		Debug.Log ("プレイヤーのHPは" + playerHP);
+	}
+}
